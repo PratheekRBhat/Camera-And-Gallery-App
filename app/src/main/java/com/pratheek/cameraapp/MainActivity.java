f431c741afc3fc31b5263ac0ae4303f5c3163489
0martel <pratheek.bht@gmail.com>
2021-05-06 23:13:58 +0530

    Added video recording option. Fixed bugs.

--- app/src/main/java/com/pratheek/cameraapp/MainActivity.java
+++ app/src/main/java/com/pratheek/cameraapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.pratheek.cameraapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
@@ -13,6 +14,8 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -20,6 +23,7 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -30,13 +34,14 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int CAMERA_PERMISSION_CODE = 101;
-    public static final int CAMERA_REQUEST_CODE = 102;
-    public static final int GALLERY_REQUEST_CODE = 105;
+    static final int CAMERA_PERMISSION_CODE = 101;
+    static final int CAMERA_REQUEST_CODE = 102;
+    static final int GALLERY_REQUEST_CODE = 105;
+    static final int VIDEO_CAPTURE_REQUEST_CODE = 103;
 
     private ImageView selectedImage;
-    private Button galleryBtn;
     String currentPhotoPath;
+    String currentVideoPath;
     String TAG = "MainActivity";
 
     @Override
@@ -45,10 +50,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         selectedImage = findViewById(R.id.displayImageView);
-        Button cameraBtn = findViewById(R.id.cameraBtn);
-        galleryBtn = findViewById(R.id.galleryBtn);
+        ImageView cameraBtn = findViewById(R.id.cameraBtn);
+        ImageView videoBtn = findViewById(R.id.videoBtn);
+        Button galleryBtn = findViewById(R.id.galleryBtn);
 
-        cameraBtn.setOnClickListener(view -> getCameraPermissions());
+        cameraBtn.setOnClickListener(view -> getCameraPermissions(CAMERA_REQUEST_CODE));
+
+        videoBtn.setOnClickListener(view -> getCameraPermissions(VIDEO_CAPTURE_REQUEST_CODE));
 
         galleryBtn.setOnClickListener(view -> {
             Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -56,11 +64,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void getCameraPermissions() {
+    public void getCameraPermissions(int requestCode) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
         } else {
-            dispatchTakePictureIntent();
+            if (requestCode == CAMERA_REQUEST_CODE)
+                dispatchTakePictureIntent();
+            else if (requestCode == VIDEO_CAPTURE_REQUEST_CODE)
+                dispatchTakeVideoIntent();
         }
     }
 
@@ -73,6 +84,80 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.example.android.camerafileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+        File videoFile = null;
+        try {
+            videoFile = createVideoFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        if (videoFile != null) {
+            Uri videoUri = FileProvider.getUriForFile(this,
+                    "com.example.android.camerafileprovider",
+                    videoFile);
+            takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, videoUri);
+            startActivityForResult(takeVideoIntent, VIDEO_CAPTURE_REQUEST_CODE);
+        }
+    }
+
+
+    @SuppressLint("SimpleDateFormat")
+    private File createVideoFile() throws IOException {
+        // Create an video file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String videoFileName = "MP4_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File video = File.createTempFile(
+                videoFileName,  /* prefix */
+                ".mp4",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentVideoPath = video.getAbsolutePath();
+        return video;
     }
 
     @SuppressLint("SimpleDateFormat")
@@ -94,6 +179,20 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        if (requestCode == VIDEO_CAPTURE_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                File f = new File(currentVideoPath);
+                Bitmap thumbnailBM = ThumbnailUtils.createVideoThumbnail(f.getAbsolutePath().toString(), MediaStore.Images.Thumbnails.MINI_KIND);
+                selectedImage.setImageBitmap(thumbnailBM);
+                Log.d(TAG, "Absolute Url of Video is " + Uri.fromFile(f));
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+            }
+        }
+
         if (requestCode == GALLERY_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
                 Uri contentUri = data != null ? data.getData() : null;
@@ -103,8 +202,6 @@ public class MainActivity extends AppCompatActivity {
                 selectedImage.setImageURI(contentUri);
             }
         }
-
-
     }
 
     private String getFileExt(Uri contentUri) {
@@ -112,46 +209,4 @@ public class MainActivity extends AppCompatActivity {
         MimeTypeMap mime = MimeTypeMap.getSingleton();
         return mime.getExtensionFromMimeType(c.getType(contentUri));
     }
-
-    @SuppressLint("SimpleDateFormat")
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-
-    @SuppressLint("QueryPermissionsNeeded")
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.camera.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-            }
-        }
-    }
-
 }
