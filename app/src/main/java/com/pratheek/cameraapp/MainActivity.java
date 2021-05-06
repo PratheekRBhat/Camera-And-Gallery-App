3fa74203cddaf7e475abeae3ba01e4ea56896c96
0martel <pratheek.bht@gmail.com>
2021-05-06 23:35:16 +0530

    Added comments explaining the different methods and their uses.

--- app/src/main/java/com/pratheek/cameraapp/MainActivity.java
+++ app/src/main/java/com/pratheek/cameraapp/MainActivity.java
@@ -1,13 +1,5 @@
 package com.pratheek.cameraapp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -23,9 +15,15 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.IOException;
@@ -49,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Attaches UI elements to the backend
         selectedImage = findViewById(R.id.displayImageView);
         ImageView cameraBtn = findViewById(R.id.cameraBtn);
         ImageView videoBtn = findViewById(R.id.videoBtn);
@@ -59,11 +58,16 @@ public class MainActivity extends AppCompatActivity {
         videoBtn.setOnClickListener(view -> getCameraPermissions(VIDEO_CAPTURE_REQUEST_CODE));
 
         galleryBtn.setOnClickListener(view -> {
+            /*
+            * Intents are normally used to move between Activities within an application or to pass the action to another application.
+            * In this case, we are passing the action of accessing the saved images to the gallery app
+            */
             Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
         });
     }
 
+    //Method to check if permission to access the camera is given. If yes, it opens the camera. If not, it asks for the permission
     public void getCameraPermissions(int requestCode) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
@@ -75,17 +79,27 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+    *This method handles the aftermath of the permission request.
+    *  If the permission is given, it opens the app, else it sends a messages asking the user to give the permission.
+    */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if(requestCode == CAMERA_PERMISSION_CODE){
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 dispatchTakePictureIntent();
             }else {
+                //Toasts are temporary displays which show a message for a certain period of time and then fades away. Duration can be adjusted by changing LENGTH_SHORT below.
                 Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
+    /*
+    * Intents are normally used to move between Activities within an application or to pass the action to another application.
+    * In this case, we are passing the action of taking a photo to the camera app
+    * This method sends the intent to the camera app while also creating a file to save the picture taken.
+    */
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         // Create the File where the photo should go
@@ -106,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Method to create an a file to save the image with a name and extension.
     @SuppressLint("SimpleDateFormat")
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -123,6 +138,11 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
+    /*
+     * Intents are normally used to move between Activities within an application or to pass the action to another application.
+     * In this case, we are passing the action of taking a video to the camera app
+     * This method sends the intent to the camera app while also creating a file to save the video taken.
+     */
     private void dispatchTakeVideoIntent() {
         Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
 
@@ -142,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    //Method to create an a file to save the image with a name and extension.
     @SuppressLint("SimpleDateFormat")
     private File createVideoFile() throws IOException {
         // Create an video file name
@@ -160,17 +180,25 @@ public class MainActivity extends AppCompatActivity {
         return video;
     }
 
+    /*
+    * Once an intent is passed to an external application, if the action was temporary, then after it is completed, our app opens again.
+    * The below method is used to perform certain operations once the user returns to our app
+    */
     @SuppressLint("SimpleDateFormat")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        //If user return from Camera app after taking a photo, save the image and attach it to the ImageView to be displayed.
         if (requestCode == CAMERA_REQUEST_CODE) {
+            // Checks if user returns to our app after successfully performing the required action.
             if (resultCode == Activity.RESULT_OK) {
                 File f = new File(currentPhotoPath);
                 selectedImage.setImageURI(Uri.fromFile(f));
                 Log.d(TAG, "Absolute Url of Image is " + Uri.fromFile(f));
 
+                /* These 4 lines of code are used to inform the gallery app or any other application that is listening
+                that a new image file has been created and it can be accessed by them.*/
                 Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
                 Uri contentUri = Uri.fromFile(f);
                 mediaScanIntent.setData(contentUri);
@@ -179,13 +207,17 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        //If user return from Camera app after taking a video, save the image and attach it to the ImageView to be displayed.
         if (requestCode == VIDEO_CAPTURE_REQUEST_CODE) {
+            // Checks if user returns to our app after successfully performing the required action.
             if (resultCode == Activity.RESULT_OK) {
                 File f = new File(currentVideoPath);
-                Bitmap thumbnailBM = ThumbnailUtils.createVideoThumbnail(f.getAbsolutePath().toString(), MediaStore.Images.Thumbnails.MINI_KIND);
+                Bitmap thumbnailBM = ThumbnailUtils.createVideoThumbnail(f.getAbsolutePath(), MediaStore.Images.Thumbnails.MINI_KIND);
                 selectedImage.setImageBitmap(thumbnailBM);
                 Log.d(TAG, "Absolute Url of Video is " + Uri.fromFile(f));
 
+                /* These 4 lines of code are used to inform the gallery app or any other application that is listening
+                that a new video file has been created and it can be accessed by them.*/
                 Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
                 Uri contentUri = Uri.fromFile(f);
                 mediaScanIntent.setData(contentUri);
@@ -193,6 +225,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        //If user return from Gallery app after choosing a file, save the image and attach it to the ImageView to be displayed.
         if (requestCode == GALLERY_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
                 Uri contentUri = data != null ? data.getData() : null;
@@ -204,6 +237,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Method to get external file's location.
     private String getFileExt(Uri contentUri) {
         ContentResolver c = getContentResolver();
         MimeTypeMap mime = MimeTypeMap.getSingleton();
